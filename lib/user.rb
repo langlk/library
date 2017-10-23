4d7e396192024fe5feb09635d1a8758a81c9d381
Kelsey Langlois <kels.langlois@gmail.com>
2017-10-23 09:55:07 -0700

    Add user-patron association.

--- lib/user.rb
+++ lib/user.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 class User < ActiveRecord::Base
+  belongs_to :patron
 
   def check_password?(input)
     self.password == input
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,18 +2,29 @@
 
 require "spec_helper"
 
-describe('User') do
-  let(:test_user) { User.new({
+describe 'User' do
+  let :user { User.new({
     email: "bob@email.com",
     username: "bobsmith",
     password: "password123",
   patron_id: 1})  }
 
 
-  describe('#check_password?') do
+  describe '#check_password?' do
     it "returns true if the input matches the password" do
-      expect(test_user.check_password?("password123")).to eq(true)
+      expect(user.check_password?("password123")).to eq(true)
     end
   end
 
+  describe '#patron' do
+    it "returns the patron a user account is connected to" do
+      patron = Patron.create({
+        first_name: "Bob",
+        last_name: "Smith"
+      })
+      user.patron_id = patron.id
+      user.save
+      expect(user.patron).to eq(patron)
+    end
+  end
 end
