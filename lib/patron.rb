5f0458bd4dbbd3fc56808f828aa3b20c74edf4b9
Kelsey Langlois <kels.langlois@gmail.com>
2017-10-23 09:39:07 -0700

    Update patron-checkout association.

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,65 +1,10 @@
 #!/usr/bin/env ruby
 
-class Patron
-  attr_reader :id
-  attr_accessor :first_name, :last_name
-
-  def initialize(attributes)
-    @first_name = attributes[:first_name]
-    @last_name = attributes[:last_name]
-    @id = attributes[:id] || nil
-  end
+class Patron < ActiveRecord::Base
+  has_many :checkouts
+  has_many :books, through: :checkouts
 
   def full_name
-    @first_name + " " + @last_name
-  end
-
-  def save
-    if @id
-      DB.exec("UPDATE patrons SET first_name = '#{@first_name}', last_name = '#{@last_name}' WHERE id = #{@id};")
-    else
-      results = DB.exec("INSERT INTO patrons (first_name, last_name) VALUES ('#{@first_name}', '#{@last_name}') RETURNING id;")
-      @id = results.first["id"].to_i
-    end
-  end
-
-  def ==(other_patron)
-    (full_name == other_patron.full_name) & (@id == other_patron.id)
-  end
-
-  def self.all
-    results = DB.exec("SELECT * FROM patrons;")
-    results.map do |result|
-      Patron.new({
-        :first_name => result["first_name"],
-        :last_name => result["last_name"],
-        :id => result["id"].to_i
-      })
-    end
-  end
-
-  def delete
-    DB.exec("DELETE FROM patrons WHERE id = #{@id};")
-    DB.exec("DELETE FROM checkouts WHERE patron_id = #{@id};")
-  end
-
-  def get_checkouts
-    Checkout.find_by_patron(@id)
-  end
-  
-  def self.find(search_term)
-    results = []
-    if search_term.class == Integer
-      results = DB.exec("SELECT * FROM patrons WHERE id = #{search_term};")
-    else
-      results = DB.exec("SELECT * FROM patrons WHERE first_name = '#{search_term}' OR last_name = '#{search_term}';")
-    end
-    results.map do |result|
-      Patron.new({
-        :first_name => result["first_name"],
-        :last_name => result["last_name"],
-        :id => result["id"].to_i
-      })
-    end
+    self.first_name + " " + self.last_name
   end
 end
--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -2,75 +2,29 @@
 
 require 'spec_helper'
 
-describe('Patron') do
-  let (:patron) { Patron.new({:first_name => "Bob", :last_name => "Smith"}) }
+describe 'Patron' do
+  let :patron { Patron.new({
+    :first_name => "Bob",
+    :last_name => "Smith"
+  }) }
 
-  it "stores a patron's first & last name and id, initializing id to nil" do
-    expect(patron.id).to eq(nil)
-    expect(patron.first_name).to eq("Bob")
-    expect(patron.last_name).to eq("Smith")
-  end
-
-  describe('#full_name') do
-    it "returns a patron's first and last names together" do
+  describe "full_name" do
+    it "returns the full name of a patron, separated by spaces" do
       expect(patron.full_name).to eq("Bob Smith")
     end
   end
 
-  describe('#==') do
-    it "is equal when two patrons' first & last names and ids are the same" do
-      patron2 = Patron.new({:first_name => "Bob", :last_name => "Smith"})
-      expect(patron).to eq(patron2)
-    end
-  end
-
-  describe('#save') do
-    it "saves a patron to the database" do
+  describe "checkouts" do
+    it "returns all of a patron's checkouts" do
       patron.save
-      expect(Patron.all).to eq([patron])
-    end
-
-    it "updates a saved patron in the database" do
-      patron.save
-      patron.last_name = "Jones"
-      patron.save
-      expect(Patron.all).to eq [patron]
-    end
-  end
-
-  describe('.all') do
-    it "returns all patrons saved to the database" do
-      expect(Patron.all).to eq([])
-    end
-  end
-
-  describe '#get_checkouts' do
-    it "returns all checkout associated" do
-      patron.save
-      book2 = Book.new({:title => "Sorcerers Stone", :author_first => "J. K.", :author_last => "Rowling"})
-      book3 = Book.new({:title => "Harry Potter", :author_first => "J. K.", :author_last => "Rowling"})
-      book2.save
-      book3.save
-      record2 = book2.checkout(patron.id)
-      record3 = book3.checkout(patron.id)
-      expect(patron.get_checkouts).to eq [record2, record3]
-    end
-  end
-
-  describe('.find') do
-    it "returns all patrons whose first or last name or id matches the search term" do
-      patron.save
-      patron2 = Patron.new({:first_name => "Bob", :last_name => "Smith"})
-      patron2.save
-      expect(Patron.find(patron.id)).to eq([patron])
-    end
-  end
-
-  describe '#delete' do
-    it 'will delete patron from database' do
-      patron.save
-      patron.delete
-      expect(Patron.all).to eq []
+      checkout = Checkout.create({
+        :patron_id => patron.id,
+        :book_id => 0,
+        :checkout_date => Date.today,
+        :due_date => Date.today,
+        :checked_in => false
+      })
+      expect(patron.checkouts).to eq([checkout])
     end
   end
 end
