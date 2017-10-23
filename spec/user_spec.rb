6a734d9380f3c8a5568bdbb05c59f0ae52794a3e
Kelsey Langlois <kels.langlois@gmail.com>
2017-10-23 09:51:42 -0700

    Update user to use activerecord

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,22 +3,12 @@
 require "spec_helper"
 
 describe('User') do
-  let(:test_user) { User.new({email: "bob@email.com", username: "bobsmith", password: "password123", patron_id: 1})}
+  let(:test_user) { User.new({
+    email: "bob@email.com",
+    username: "bobsmith",
+    password: "password123",
+  patron_id: 1})  }
 
-  it "saves a user's id, email, username, password, and type" do
-    expect(test_user.id).to eq(nil)
-    expect(test_user.email).to eq("bob@email.com")
-    expect(test_user.username).to eq("bobsmith")
-    expect(test_user.admin).to eq(false)
-    expect(test_user.patron_id).to eq(1)
-  end
-
-  describe('#==') do
-    it "declares two users as equal when they have the same attributes" do
-      user2 = User.new({email: "bob@email.com", username: "bobsmith", password: "password123", patron_id: 1})
-      expect(test_user).to eq(user2)
-    end
-  end
 
   describe('#check_password?') do
     it "returns true if the input matches the password" do
@@ -26,59 +16,4 @@ describe('User') do
     end
   end
 
-  describe('#save') do
-    it "saves a user to the database" do
-      test_user.save
-      expect(User.all).to eq([test_user])
-    end
-
-    it "updates a saved user's data" do
-      test_user.save
-      test_user.make_admin
-      test_user.save
-      expect(User.find_id(test_user.id).admin).to eq(true)
-    end
-  end
-
-  describe('#make_admin') do
-    it "makes a user an admin" do
-      test_user.make_admin
-      expect(test_user.admin).to eq(true)
-    end
-  end
-
-  describe('#delete') do
-    it "removes a user from the database" do
-      test_user.save
-      test_user.delete
-      expect(User.all).to eq([])
-    end
-  end
-
-  describe(".all") do
-    it "returns a list of all users in the database" do
-      expect(User.all).to eq([])
-    end
-  end
-
-  describe('.find_id') do
-    it "finds a user by their id" do
-      test_user.save
-      expect(User.find_id(test_user.id)).to eq(test_user)
-    end
-  end
-
-  describe('.find_user') do
-    it "finds a user by their username" do
-      test_user.save
-      expect(User.find_user("bobsmith")).to eq(test_user)
-    end
-  end
-
-  describe('.find_email') do
-    it "firnds a user by their email" do
-      test_user.save
-      expect(User.find_email("bob@email.com")).to eq(test_user)
-    end
-  end
 end
